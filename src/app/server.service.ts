bf3fdf28a3b3ec035f8b610a1f3e75e42857ff82
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-11-29 18:51:00 -0500

    Adjusting Request Headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class ServerService {
@@ -9,7 +9,8 @@ export class ServerService {
     constructor(private http: Http) {}
 
     addServers(servers: any[]) {
-        return this.http.post(this.fireBaseUrl, servers);
+        const myHeaders = new Headers({'Content-Type': 'application/json'});
+        return this.http.post(this.fireBaseUrl, servers, {headers: myHeaders});
     }
 
 }
